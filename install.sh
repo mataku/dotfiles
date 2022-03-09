80633c2d7142676004592c84af93b10bea62288a
Takuma Homma <nagomimatcha@gmail.com>
2022-03-09 23:21:21 +0900

    mataku

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-ln -sf ~/src/github.com/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/src/github.com/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/src/github.com/dotfiles/vim/autoload ~/.vim
-ln -sf ~/src/github.com/dotfiles/vim/colors ~/.vim
-ln -sf ~/src/github.com/dotfiles/vim/after ~/.vim
-ln -sf ~/src/github.com/dotfiles/vim/indent ~/.vim
-ln -sf ~/src/github.com/dotfiles/vim/bundle ~/.vim
-ln -sf ~/src/github.com/dotfiles/vim/rc ~/.vim
-ln -sf ~/src/github.com/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/src/github.com/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/src/github.com/dotfiles/powerline ~/.config/powerline
-ln -sf ~/src/github.com/dotfiles/fish ~/.config/fish
-ln -sf ~/src/github.com/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/src/github.com/dotfiles/.irbrc ~/.irbrc
-ln -sf ~/src/github.com/dotfiles/emacs.d/init.el ~/.emacs.d/init.el
-ln -sf ~/src/github.com/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/src/github.com/dotfiles/nvim/rc ~/.config/nvim/rc
-ln -sf ~/src/github.com/dotfiles/nvim/coc/package.json ~/.config/coc/extensions/package.json
+ln -sf ~/src/github.com/mataku/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/src/github.com/mataku/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/src/github.com/mataku/dotfiles/vim/autoload ~/.vim
+ln -sf ~/src/github.com/mataku/dotfiles/vim/colors ~/.vim
+ln -sf ~/src/github.com/mataku/dotfiles/vim/after ~/.vim
+ln -sf ~/src/github.com/mataku/dotfiles/vim/indent ~/.vim
+ln -sf ~/src/github.com/mataku/dotfiles/vim/bundle ~/.vim
+ln -sf ~/src/github.com/mataku/dotfiles/vim/rc ~/.vim
+ln -sf ~/src/github.com/mataku/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/src/github.com/mataku/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/src/github.com/mataku/dotfiles/powerline ~/.config/powerline
+ln -sf ~/src/github.com/mataku/dotfiles/fish ~/.config/fish
+ln -sf ~/src/github.com/mataku/dotfiles/.tigrc ~/.tigrc
+ln -sf ~/src/github.com/mataku/dotfiles/.irbrc ~/.irbrc
+ln -sf ~/src/github.com/mataku/dotfiles/emacs.d/init.el ~/.emacs.d/init.el
+ln -sf ~/src/github.com/mataku/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/src/github.com/mataku/dotfiles/nvim/rc ~/.config/nvim/rc
+ln -sf ~/src/github.com/mataku/dotfiles/nvim/coc/package.json ~/.config/coc/extensions/package.json
 
 mkdir -p ~/.config/alacritty
-ln -sf ~/src/github.com/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/src/github.com/mataku/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
-ln -sf ~/src/github.com/dotfiles/xcode-theme/Material\ Dark.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Material\ Dark.xccolortheme
+ln -sf ~/src/github.com/mataku/dotfiles/xcode-theme/Material\ Dark.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Material\ Dark.xccolortheme
