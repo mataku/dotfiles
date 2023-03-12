5e001abf8f43eaf1a7a854558ae7f9e487f030a2
Takuma Homma <nagomimatcha@gmail.com>
2023-03-12 20:24:00 +0900

    Apply if exists

--- fish/env.fish
+++ fish/env.fish
@@ -1,5 +1,6 @@
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set -x PATH /usr/local/bin $PATH
+set -x PATH $HOME/.nodenv/shims $PATH
 
 if test (uname -m) = 'arm64'
   set -x PATH /opt/homebrew/bin $PATH
@@ -7,7 +8,7 @@ end
 # set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 set -x PATH $HOME/Library/Android/sdk/tools $PATH
 set -x GOPATH $HOME/go
-set -x EDITOR "emacsclient -nw -t"
+set -x EDITOR "nvim"
 set fish_greeting ""
 set -x HOMEBREW_NO_AUTO_UPDATE 1
 # set -x PATH $HOME/src/github.com/flutter/flutter/bin $PATH
@@ -20,4 +21,9 @@ set fish_color_error FF7043
 set -x FZF_DEFAULT_OPTS '--cycle --ansi --select-1 --exit-0'
 set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 
+
 . ~/.config/fish/environments/android.fish
+
+if test -e /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+  . "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+end
