391d853ea3ec2babb037ebad168261ca9f8a44ab
Takuma Homma <nagomimatcha@gmail.com>
2023-03-12 20:21:04 +0900

    adb shell wrapper

--- fish/functions/adb-open.fish
+++ fish/functions/adb-open.fish
@@ -1,10 +1,8 @@
-function adb-open
-  set -l package (adb shell pm list package | sed -e s/package:// | fzf)
-  if [ $package ]
-    set -l activity (adb shell pm dump $package | grep -A 2 android.intent.action.MAIN | head -2 | tail -1 | awk '{print $2}')
-    adb shell am start -n $activity
+function adb-open-app
+  if [ $argv[1] ]
+    adb shell am start -a android.intent.action.VIEW -d $argv[1]
   else
-    echo 'Specify app!'
+    echo 'Specify scheme!'
     return 1
   end
 end
