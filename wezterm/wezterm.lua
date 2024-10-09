a5ccc7565d579346a63dccdbc25ea8753611cbb9
Takuma Homma <nagomimatcha@gmail.com>
2024-10-10 00:09:09 +0900

    Bring from iTerm2 colorscheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,10 +4,37 @@ local config = wezterm.config_builder()
 
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
 
--- https://wezfurlong.org/wezterm/colorschemes/m/index.html
--- config.color_scheme = 'Material (base16)'
-config.color_scheme = 'OceanicNext (base16)'
--- config.color_scheme = 'Codeschool (dark) (terminal.sexy)'
+config.colors = {
+  foreground = '#eceef0',
+  background = '#283137',
+  cursor_bg = '#edeeed',
+  cursor_border = '#eaeaea',
+  cursor_fg = '#000000',
+  selection_bg = '#657c89',
+  selection_fg = '#eceef0',
+
+  ansi = {
+    '#596d78',
+    '#eb5f59',
+    '#8eedb3',
+    '#f8d85e',
+    '#68c1f9',
+    '#eb5181',
+    '#8ff8db',
+    '#fefefe',
+  },
+
+  brights = {
+    '#b2bdc4',
+    '#ef9084',
+    '#c5f4cd',
+    '#fae58d',
+    '#94d5fa',
+    '#ee86aa',
+    '#bafaeb',
+    '#fefefe',
+  }
+}
 
 config.font = wezterm.font_with_fallback {
   'Cica'
