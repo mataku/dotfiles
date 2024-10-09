e2edd68ac31d4ef2e6a3c4bb1709c5206286c6b0
Takuma Homma <nagomimatcha@gmail.com>
2024-10-10 01:39:56 +0900

    Show simple current dir of active pane

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -52,8 +52,46 @@ config.keys = {
   { key = 'RightArrow', mods = 'CMD', action = act.MoveTabRelative(1) },
 }
 
+function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+function split_path(path)
+  local result = {}
+  for part in string.gmatch(path, "([^/]+)") do
+    table.insert(result, part)
+  end
+  return result
+end
+
+function prompt_pwd(current_dir)
+  local simple_home_path = string.gsub(current_dir, "^/Users/mataku", '~')
+
+  local path_array = split_path(simple_home_path)
+  local result = ""
+  local path_array_size = #path_array
+
+  for i, val in ipairs(path_array) do
+    if (result == '') then
+      result = result .. string.sub(val, 1, 1)
+    elseif (i == path_array_size) then
+      result = result .. '/' .. val
+    else
+      result = result .. '/' .. string.sub(val, 1, 1)
+    end
+  end
+
+  return result
+end
+
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
-  return 'WezTerm'
+  return prompt_pwd(pane.current_working_dir.path)
+end)
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local active_pane = tab.active_pane
+  local process_name = active_pane.foreground_process_name
+  return basename(active_pane.current_working_dir.path) .. ' ' .. '(' .. basename(process_name) .. ')'
 end)
 
 return config
