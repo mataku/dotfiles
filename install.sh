9342430140a52fc3c1b85aa680081396a46623c7
Takuma Homma <nagomimatcha@gmail.com>
2024-10-09 22:18:27 +0900

    path

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ ln -sf ~/src/github.com/mataku/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/src/github.com/mataku/dotfiles/fish ~/.config/fish
 ln -sf ~/src/github.com/mataku/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/src/github.com/mataku/dotfiles/.irbrc ~/.irbrc
-ln -sf ~/src/github.com/mataku/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/src/github.com/mataku/dotfiles/nvim/rc/dein.toml ~/.config/nvim/rc/dein.toml
-ln -sf ~/src/github.com/mataku/dotfiles/nvim/rc/dein_lazy.toml ~/.config/nvim/rc/dein_lazy.toml
-ln -sf ~/src/github.com/mataku/dotfiles/nvim/coc/package.json ~/.config/coc/extensions/package.json
+ln -sf ~/src/github.com/mataku/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/src/github.com/mataku/dotfiles/nvim/ ~/.config/nvim/init.lua
+ln -sf ~/src/github.com/mataku/dotfiles/nvim/lua/config/lazy.lua ~/.config/nvim/lua/config/lazy.lua
 ln -sf ~/src/github.com/mataku/dotfiles/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
