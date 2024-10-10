ffcbfe73b4136e6979038af3e8882e1ede2acaad
Takuma Homma <nagomimatcha@gmail.com>
2024-10-10 23:06:14 +0900

    always use command+t to create new tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -46,6 +46,9 @@ config.enable_scroll_bar = false
 config.show_tab_index_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
+config.show_new_tab_button_in_tab_bar = false
+-- config.show_close_tab_button_in_tabs = false
+
 local act = wezterm.action
 
 config.keys = {
