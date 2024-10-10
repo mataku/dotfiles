c6282c2ab52bd63cdb3abb5c8d4c6d9f6d4c7f7e
Takuma Homma <nagomimatcha@gmail.com>
2024-10-10 23:05:10 +0900

    No single tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -44,6 +44,7 @@ config.adjust_window_size_when_changing_font_size = false
 
 config.enable_scroll_bar = false
 config.show_tab_index_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
 local act = wezterm.action
 
