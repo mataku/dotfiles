ca728d8a575821c80100f165eaec75bcf5c03c93
Takuma Homma <nagomimatcha@gmail.com>
2025-12-11 22:15:41 +0900

    Use argument if specified

--- fish/functions/adb-open-app.fish
+++ fish/functions/adb-open-app.fish
@@ -1,4 +1,10 @@
 function adb-open-app
+  if [ $argv[1] ]
+    set -l activity (adb shell pm dump $argv[1] | grep -A 2 android.intent.action.MAIN | head -2 | tail -1 | awk '{print $2}')
+    adb shell am start -n $activity
+    return 0
+  end
+
   set -l package (adb shell pm list package | sed -e s/package:// | fzf)
   if [ $package ]
     set -l activity (adb shell pm dump $package | grep -A 2 android.intent.action.MAIN | head -2 | tail -1 | awk '{print $2}')
