194261da9b1aff059ce5276ae6c8b22fe8af252f
Takuma Homma <nagomimatcha@gmail.com>
2025-12-11 22:15:54 +0900

    Only darwin

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+  config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+end
+
 
 config.colors = {
   foreground = '#eceef0',
