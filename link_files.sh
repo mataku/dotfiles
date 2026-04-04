47450646499087b69f54051f5604a017797168c0
Takuma Homma <nagomimatcha@gmail.com>
2026-04-04 23:49:30 +0900

    Refine tmux settings

--- link_files.sh
+++ link_files.sh
@@ -9,3 +9,14 @@ ln -sf ~/src/github.com/mataku/dotfiles/nvim/lua/plugins  ~/.config/nvim/lua/plu
 ln -sf ~/src/github.com/mataku/dotfiles/nvim/lua/config/lazy.lua ~/.config/nvim/lua/config/lazy.lua
 ln -sf ~/src/github.com/mataku/dotfiles/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
 ln -sf ~/src/github.com/mataku/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+
+# Pre-nix migration: ZDOTDIR must be set in ~/.zshenv (zsh requirement)
+# In nix, this is managed by home-manager (see nix/home/default.nix)
+echo 'export ZDOTDIR="$HOME/.config/zsh"' > ~/.zshenv
+mkdir -p ~/.config/zsh/environments
+ln -sf ~/src/github.com/mataku/dotfiles/zsh/.zshrc ~/.config/zsh/.zshrc
+ln -sf ~/src/github.com/mataku/dotfiles/zsh/alias.zsh ~/.config/zsh/alias.zsh
+ln -sf ~/src/github.com/mataku/dotfiles/zsh/env.zsh ~/.config/zsh/env.zsh
+ln -sf ~/src/github.com/mataku/dotfiles/zsh/prompt.zsh ~/.config/zsh/prompt.zsh
+ln -sf ~/src/github.com/mataku/dotfiles/zsh/functions.zsh ~/.config/zsh/functions.zsh
+ln -sf ~/src/github.com/mataku/dotfiles/zsh/environments/android.zsh ~/.config/zsh/environments/android.zsh
